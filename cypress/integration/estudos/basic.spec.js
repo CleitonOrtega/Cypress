9e84e74beff6fc78574dbf02efbec3ad7d170703
C. Ortega <c.ortega200935@gmail.com>
2023-06-21 15:10:15 -0300

    Localizar e interagir com elementos e Debug/Pause

--- cypress/integration/estudos/basic.spec.js
+++ cypress/integration/estudos/basic.spec.js
@@ -1,13 +1,15 @@
 /// <reference types="cypress" />
 
 describe('Cypress basics', () =>{
-    it('Should visit a page and assert title', () =>{
+    it.only('Should visit a page and assert title', () =>{
         cy.visit('https://wcaquino.me/cypress/componentes.html')
         //const title = cy.title()
         //console.log(title)
 
+        cy.pause()
+
         cy.title().should('be.equal', 'Campo de Treinamento')
-        cy.title().should('contain', 'Campo')
+        cy.title().debug().should('contain', 'Campo')
 
         cy.title()
             .should('be.equal', 'Campo de Treinamento')
@@ -15,5 +17,15 @@ describe('Cypress basics', () =>{
 
         //TODO imprimir o log no console
         //TODO escrever o log em um campo de texto
+    })
+
+    it('Should find and interact with an element', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+        cy.get('#buttonSimple')
+            .click()
+            .should('have.value', 'Obrigado!')
+
+
     })
 })
